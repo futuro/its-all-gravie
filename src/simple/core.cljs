3f644ac807be47454f003344425ba41b7c851f27
Evan Niessen-Derry <eniessenderry@gmail.com>
2023-08-25 15:20:53 -0500

    Tweak core cljs file to be Gravie specific
    This removes most of the original "simple" example code, leaving the code I've added for the Gravie
    test-project.

--- src/simple/core.cljs
+++ src/simple/core.cljs
@@ -1,81 +1,62 @@
 (ns simple.core
+  "This namespace was originally copied from the re-frame \"simple\" example app, specifically this
+  file:
+  https://github.com/day8/re-frame/blob/b33956ef15f09bf6200fc0f97e9cb1db87e3a3cc/examples/simple/src/simple/core.cljs
+
+  The reasoning behind this decision is laid out in the `dev-log.org` file, but I have since altered
+  it almost completely. Still, I wanted to give credit where it's due, for getting me off the
+  ground."
   (:require [reagent.dom :as rdom]
             [re-frame.core :as rf]
             [day8.re-frame.tracing :refer-macros [fn-traced]]))
 
-;; A detailed walk-through of this source code is provided in the docs:
-;; https://day8.github.io/re-frame/dominoes-live/
+;; Helpers
 
-;; -- Domino 1 - Event Dispatch -----------------------------------------------
-
-(defn dispatch-timer-event
-  []
-  (let [now (js/Date.)]
-    (rf/dispatch [:timer now])))  ;; <-- dispatch used
-
-;; Call the dispatching function every second.
-;; `defonce` is like `def` but it ensures only one instance is ever
-;; created in the face of figwheel hot-reloading of this file.
-;(defonce do-timer (js/setInterval dispatch-timer-event 10000))
+(defn event->value
+  "Given an event, returns the event's target's value."
+  [e]
+  (-> e .-target .-value))
 
 ;; -- Domino 2 - Event Handlers -----------------------------------------------
 
 (rf/reg-event-db              ;; sets up initial application state
  :initialize                 ;; usage:  (dispatch [:initialize])
  (fn [_ _]                   ;; the two parameters are not important here, so use _
-   {:time (js/Date.)         ;; What it returns becomes the new application state
-    :time-color "orange"}))  ;; so the application state will initially be a map with two keys
+   {:search-term    ""
+    :search-results []
+    :cart           []
+    :rented-items   []}))
 
-(rf/reg-event-db                ;; usage:  (dispatch [:time-color-change 34562])
- :time-color-change            ;; dispatched when the user enters a new colour into the UI text field
- (fn-traced [db [_ new-color-value]]  ;; -db event handlers given 2 parameters:  current application state and event (a vector)
-   (assoc db :time-color new-color-value)))   ;; compute and return the new application state
-
-(rf/reg-event-db                 ;; usage:  (dispatch [:timer a-js-Date])
- :timer                         ;; every second an event of this kind will be dispatched
- (fn [db [_ new-time]]          ;; note how the 2nd parameter is destructured to obtain the data value
-   (assoc db :time new-time)))  ;; compute and return the new application state
+(rf/reg-event-db
+ :search-term-change
+ (fn [db [_ new-search-term]]
+   (assoc db :search-term new-search-term)))
 
 ;; -- Domino 4 - Query  -------------------------------------------------------
 
 (rf/reg-sub
- :time
- (fn [db _]     ;; db is current app state. 2nd unused param is query vector
-   (:time db))) ;; return a query computation over the application state
-
-(rf/reg-sub
- :time-color
+ :search-term
  (fn [db _]
-   (:time-color db)))
+   (:search-term db)))
 
 ;; -- Domino 5 - View Functions ----------------------------------------------
 
-(defn clock
+(defn search-input
   []
-  (let [colour @(rf/subscribe [:time-color])
-        time   (-> @(rf/subscribe [:time])
-                   .toTimeString
-                   (clojure.string/split " ")
-                   first)]
-    [:div.example-clock {:style {:color colour}} time]))
-
-(defn color-input
-  []
-  (let [gettext (fn [e] (-> e .-target .-value))
-        emit    (fn [e] (rf/dispatch [:time-color-change (gettext e)]))]
-    [:div.color-input
-     "Display color: "
-     [:input {:type "text"
+  (let [emit        (fn [e] (rf/dispatch [:search-term-change (event->value e)]))
+        search-term @(rf/subscribe [:search-term])]
+    [:div
+     "Search Term: "
+     [:input {:type  "text"
               :style {:border "1px solid #CCC"}
-              :value @(rf/subscribe [:time-color])        ;; subscribe
-              :on-change emit}]]))  ;; <---
+              :value @(rf/subscribe [:search-term])
+              :on-change emit}]]))
 
 (defn ui
   []
   [:div
-   [:h1 "The time is now:"]
-   [clock]
-   [color-input]])
+   [:h1 "BlockBuster Forever"]
+   [search-input]])
 
 ;; -- Entry Point -------------------------------------------------------------
 
