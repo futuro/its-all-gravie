bca5a23307e08246244e2c79922caf0e6be7845b
Evan Niessen-Derry <eniessenderry@gmail.com>
2023-08-29 11:21:12 -0500

    Add build file
    This grabs the necessary dependencies for creating a production release with shadow-cljs, enabling
    builds in environments that don't already have a JDK (like Cloudflare Pages).

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Beginning build"
+
+echo "Fetching JDK"
+# Grab the java binary and put it in our path
+wget https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.8%2B7/OpenJDK17U-jre_x64_linux_hotspot_17.0.8_7.tar.gz
+# When transform the initial directory name into something static, to make it easier to work with
+tar --gzip --transform 's,^[^/]+,jdk,x' --extract --file OpenJDK17U-jre_x64_linux_hotspot_17.0.8_7.tar.gz
+export PATH=$PWD/jdk/bin:$PATH
+
+echo "Building release"
+npx shadow-cljs release :client
