d49bd9a7e50159919c45f75d1e3f9c40c1b91ed5
Evan Niessen-Derry <eniessenderry@gmail.com>
2023-09-01 17:08:49 -0500

    Flesh out the rest of the skeleton for the home page
    Once I've got a sense for the shape of the game data we can build a grid of cards to show the games
    that have been rented.

--- src/software/justenough/itsallgravie/home.cljs
+++ src/software/justenough/itsallgravie/home.cljs
@@ -1,7 +1,14 @@
 (ns software.justenough.itsallgravie.home
   (:require [reagent-mui.material.container :refer [container]]
+            [reagent-mui.material.divider :refer [divider]]
             [reagent-mui.material.typography :refer [typography]]
             [software.justenough.itsallgravie.api-key :as api-key]))
+
+(defn rented-games
+  []
+  [typography
+   {:variant :h3}
+   "Currently Checked Out Games"])
 
 (defn page
   []
@@ -13,4 +20,7 @@
    [typography
     {:variant :h3}
     "Welcome back!"]
-   [api-key/input]])
+   [api-key/input]
+   [divider {:flexItem true
+             :sx {:my "10px"}}]
+   [rented-games]])
