c95701989ed5b19d80887427f88745b41ad3afa2
Evan Niessen-Derry <eniessenderry@gmail.com>
2023-09-01 16:21:24 -0500

    Leverage MUI for the API key UI
    Primarily because it looks nicer, secondarily because it fits the other UI elements.

--- src/software/justenough/itsallgravie/api_key.cljs
+++ src/software/justenough/itsallgravie/api_key.cljs
@@ -2,11 +2,13 @@
   "This contains the various functions for handling the api-key."
   (:require [reagent.dom :as rdom]
             [re-frame.core :as rf]
+            [reagent-mui.material.text-field :refer [text-field]]
             ;; Needed for the `:fetch` effect handler
             [superstructor.re-frame.fetch-fx]
             [software.justenough.itsallgravie.utils :as utils]
             [day8.re-frame.tracing :refer-macros [fn-traced]]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [reagent-mui.material.typography :refer [typography]]))
 
 (rf/reg-event-db
  ::change
@@ -23,9 +25,13 @@
   (let [emit    (fn [e] (rf/dispatch [::change (utils/event->value e)]))
         api-key @(rf/subscribe [::value])]
     [:div
-     "API Key: "
-     [:input {:type      "password"
-              :style     {:border "1px solid #CCC"}
-              :value     api-key
-              :on-change emit}]]))
+     {:style {:display :flex
+              :flex-direction :column}}
+     [typography {:variant :body1}
+      "Enter in your Giant Bomb API key to start renting games!"]
+     [text-field {:label "API Key"
+                  :variant "outlined"
+                  :value api-key
+                  :type :password
+                  :on-change emit}]]))
 
