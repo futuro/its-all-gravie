a229532020b480edaad25ac9d6bb2763f6f5f349
Evan Niessen-Derry <eniessenderry@gmail.com>
2023-09-03 20:37:52 -0500

    Customize local storage interceptor
    The short of it is that the default interceptor merges the local storage over the live db, losing
    data that's not already in the local storage.
    
    The docstring has some more information on what I would do long term with this interceptor.

--- src/software/justenough/itsallgravie/db.cljs
+++ src/software/justenough/itsallgravie/db.cljs
@@ -12,10 +12,32 @@
    :games
    :rented-games])
 
+(defn persist-db-keys
+  "This is an almost exact replica of `local-storage/persist-db-keys`, except that I've changed the
+  `:before` function to merge the existing DB on top of the local storage data, instead of what it
+  was doing before, which was merging the local store data on top of the live data.
+
+  This was causing the collection of games to get overwritten with an empty set, which then
+  invalidated our data.
+
+  Longer term, I'd either submit a bug fix to the local storage library, or write my own
+  interceptor, given how little code is required."
+  [store-key db-keys]
+  (local-storage/register-store store-key)
+  (rf/->interceptor
+    :id (keyword (str (apply str (sort db-keys)) "->" store-key))
+    :before (fn [context]
+              (update-in context [:coeffects :db] #(merge (local-storage/<-store store-key) %)))
+    :after (fn [context]
+             (when-let [value (some-> (get-in context [:effects :db])
+                                      (select-keys db-keys))]
+               (local-storage/->store store-key value))
+             context)))
+
 (def persist-db-intercepter
   "An intercepter that merges in the local storage values as a `:before` action, and then persists the
   given keys back into local storage `:after`."
-  (local-storage/persist-db-keys LOCAL-STORAGE-KEY db-keys-to-persist))
+  (persist-db-keys LOCAL-STORAGE-KEY db-keys-to-persist))
 
 (rf/reg-event-db
  ::initialize
