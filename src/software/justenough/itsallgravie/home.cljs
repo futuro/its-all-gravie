661df329ed7f0424c7cd56a99b50339478be3ce2
Evan Niessen-Derry <eniessenderry@gmail.com>
2023-09-03 22:28:24 -0500

    Add the borrowed games as cards to home screen
    This also includes a couple todos for myself on final tweaks I'd like to make before being "done".

--- src/software/justenough/itsallgravie/home.cljs
+++ src/software/justenough/itsallgravie/home.cljs
@@ -2,13 +2,21 @@
   (:require [reagent-mui.material.container :refer [container]]
             [reagent-mui.material.divider :refer [divider]]
             [reagent-mui.material.typography :refer [typography]]
-            [software.justenough.itsallgravie.api-key :as api-key]))
+            [software.justenough.itsallgravie.api-key :as api-key]
+            [re-frame.core :as rf]
+            [software.justenough.itsallgravie.game :as game]))
 
 (defn rented-games
   []
-  [typography
-   {:variant :h3}
-   "Currently Checked Out Games"])
+  (let [borrowed-games @(rf/subscribe [::game/rented-games])]
+    [container
+     [typography
+      {:variant :h3}
+      "Currently Checked Out Games"]
+     ;; TODO:
+     ;; - Tweak the sizing of the cards?
+     ;; - Alter the text on each card to a "return game" text
+     [game/grid borrowed-games]]))
 
 (defn page
   []
