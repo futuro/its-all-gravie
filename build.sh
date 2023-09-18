d8fc350376a82a9a1712d42eceb8b5decc0c015a
Evan Niessen-Derry <eniessenderry@gmail.com>
2023-09-18 13:05:20 -0500

    Switch build system over to asdf
    `jenv` was nice at first, in that it made it super simple to specify which version of java I wanted
    to use, but once I upgraded my version of java, I also had to change my invocation of jenv to
    register the new version.
    
    That was a lot of tedium, so instead I've decided to manage my java versions with ASDF, especially
    because it can manage Node versions as well.
    
    Furthermore, I believe the build environment in Cloudflare has ASDF installed, so I'm going to try
    switching the build script over to making use of it, and see what happens.

--- build.sh
+++ build.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+echo "Inspecting asdf plugins"
+asdf info
+asdf plugin list
+
 echo "Beginning build"
 
-echo "Fetching JDK"
-# Grab the java binary and put it in our path
-wget https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.8%2B7/OpenJDK17U-jre_x64_linux_hotspot_17.0.8_7.tar.gz
-# When transform the initial directory name into something static, to make it easier to work with
-tar --gzip --transform 's,^[^/]+,jdk,x' --extract --file OpenJDK17U-jre_x64_linux_hotspot_17.0.8_7.tar.gz
-export PATH=$PWD/jdk/bin:$PATH
+echo "installing asdf java plugin"
+asdf plugin add java https://github.com/halcyon/asdf-java.git
+
+echo "Installing local versions"
+asdf install
+
+# echo "Fetching JDK"
+# # Grab the java binary and put it in our path
+# wget https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.8%2B7/OpenJDK17U-jre_x64_linux_hotspot_17.0.8_7.tar.gz
+# # When transform the initial directory name into something static, to make it easier to work with
+# tar --gzip --transform 's,^[^/]+,jdk,x' --extract --file OpenJDK17U-jre_x64_linux_hotspot_17.0.8_7.tar.gz
+# export PATH=$PWD/jdk/bin:$PATH
 
 echo "Building release"
 npx shadow-cljs release :client
